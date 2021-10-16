7457dfe71d726ee9a9e01ba67a5278539081cf42
nolivercuny <nicholas.oliver85@spsmail.cuny.edu>
2021-10-16 12:58:09 -0500

    try fixing tables

--- sql/company.sql
+++ sql/company.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS company(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    id INTEGER PRIMARY KEY NOT NULL,
     company_name TEXT NOT NULL,
     company_size TEXT,
     industry TEXT
